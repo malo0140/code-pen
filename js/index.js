0f95351264099056ef65b1f060a4b2468d3865c1
malo0140 <Kara.Lynn.444@hotmail.com>
2016-02-21 02:48:57 -0500

    feat(*): add nav items

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function init(){
  TweenMax.fromTo('.cont_modal_back',1,{top:'-100%'},{top:'0%',delay:0.5});
 
+TweenMax.fromTo('.cont_text_modal',1,{top:'-100%'},{top:50,delay:1});
 }
 
 imagesLoaded( document.querySelector('.cont_principal'), function( ) {
