b831cd151aa838a38e07d9da5aa99b831a536f3f
malo0140 <Kara.Lynn.444@hotmail.com>
2016-02-25 16:16:15 -0500

    feat(js): add working nav js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function init(){
- TweenMax.fromTo('.cont_modal_back',1,{top:'-100%'},{top:'0%',delay:0.5});
+ TweenMax.fromTo('.cont_modal_back',1,{top:'-100%'},{top:'0%',delay:0.7});
 
-TweenMax.fromTo('.cont_text_modal',1,{top:'-100%'},{top:50,delay:1});
+TweenMax.fromTo('.cont_text_modal',1,{top:'-100%'},{top:50,delay:.8});
 }
 
 imagesLoaded( document.querySelector('.cont_principal'), function( ) {
